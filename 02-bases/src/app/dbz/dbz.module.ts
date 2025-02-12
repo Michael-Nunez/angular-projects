d16df857956082196d89473126a8f2908d90640a
Michael Nunez <michaeln@labreferencia.com>
2025-02-12 15:46:40 -0400

    Uso de ngModel

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AddCharacterComponent } from './components/add-character/add-character.component';
 import { MainPageComponent } from './pages/main-page.component';
@@ -15,7 +16,8 @@ import { ListComponent } from './components/list/list.component';
     MainPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class DbzModule { }
