2d3190087b570d4c93aebe2a162e80837057107a
Michael Nunez <michaeln@labreferencia.com>
2025-02-14 14:16:20 -0400

    Implementacion basica de un servicio

--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,28 +8,5 @@ import { Character } from '../interfaces/character.interface';
   standalone: false
 })
 export class MainPageComponent {
-  public characters: Character[] =
-    [
-      {
-        name: 'Krillin',
-        power: 1000
-      },
-      {
-        name: 'Goku',
-        power: 9500
-      },
-      {
-        name: 'Vegeta',
-        power: 7500
-      }
-    ];
-
-  onNewCharacter(character: Character): void {
-    this.characters.push(character);
-  }
-
-  onDeleteCharacter(index: number): void {
-    const quantityToDelete: number = 1;
-    this.characters.splice(index, quantityToDelete);
-  }
+  constructor(public dbzService: DbzService) {}
 }
