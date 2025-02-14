8dcccb92d4d83c72fe29a689fc121a5bd069ef94
Michael Nunez <michaeln@labreferencia.com>
2025-02-14 14:40:53 -0400

    Resolucion de tarea (eliminar character por id)

--- 02-bases/src/app/dbz/services/dbz.service.ts
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({
    providedIn: 'root'
@@ -8,25 +9,28 @@ export class DbzService {
   public characters: Character[] =
     [
       {
+        id: uuid(),
         name: 'Krillin',
         power: 1000
       },
       {
+        id: uuid(),
         name: 'Goku',
         power: 9500
       },
       {
+        id: uuid(),
         name: 'Vegeta',
         power: 7500
       }
     ];
 
   onNewCharacter(character: Character): void {
-    this.characters.push(character);
+    const newCharacter: Character = { id: uuid(), ...character };
+    this.characters.push(newCharacter);
   }
 
-  onDeleteCharacter(index: number): void {
-    const quantityToDelete: number = 1;
-    this.characters.splice(index, quantityToDelete);
+  deleteCharacterById(id: string): void {
+    this.characters = this.characters.filter(character => character.id !== id);
   }
 }
