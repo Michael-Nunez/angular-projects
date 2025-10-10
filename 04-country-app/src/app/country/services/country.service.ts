912790ca3a1b092dd4e8ce2d9c8d98409e7cd601
Michael Nunez <michaeln@labreferencia.com>
2025-10-10 17:14:51 -0400

    Bindeo de paises y sus descripciones con TypeScript

--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
 import type { RESTCountry } from '../interfaces/rest-countries.interface';
 import type { Country } from '../interfaces/country.interface';
@@ -33,10 +33,26 @@ export class CountryService {
     return this.httpClient.get<RESTCountry[]>(`${API_URL}/name/${query}`)
       .pipe(
         map((restCiuntries) => CountryMapper.mapRestCountryArrayToCountryArray(restCiuntries)),
+        delay(3000),
         catchError((error) => {
           console.log({error});
 
           return throwError(() => new Error(`No se pudo encontrar un país con el nombre: ${query}`));
+        })
+      );
+  }
+
+  searchByCountryByAlphaCode(code: string) {
+    code = code.toLocaleLowerCase();
+
+    return this.httpClient.get<RESTCountry[]>(`${API_URL}/alpha/${code}`)
+      .pipe(
+        map((restCiuntries) => CountryMapper.mapRestCountryArrayToCountryArray(restCiuntries)),
+        map((countries) => countries[0]),
+        catchError((error) => {
+          console.log({error});
+
+          return throwError(() => new Error(`No se pudo encontrar un país por el código: ${code}`));
         })
       );
   }
