b73d5bcf387f8982379423e363e0feaad631dc4f
Michael Nunez <michaeln@labreferencia.com>
2025-10-27 09:38:49 -0400

    Moviendo la lógica de validación de los campos del formulario a una clase utilitaria

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'basic-page',
@@ -9,6 +10,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class BasicPage {
   private formBuilder = inject(FormBuilder);
+  formUtils = FormUtils;
 
   myForm: FormGroup = this.formBuilder.group({
     //* Posiciones del arreglo: 0 -> Valor por defecto del input, 1 -> Validadores síncronos 2 -> Validadores asíncronos
@@ -16,31 +18,6 @@ export class BasicPage {
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]]
   });
-
-  isValidField(fieldName: string): boolean | null {
-    return !! (this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched);
-  }
-
-  getFieldErrors(fieldName: string): string | null {
-    if (!this.myForm.controls[fieldName]) return null;
-
-    const errors = this.myForm.controls[fieldName].errors ?? {};
-
-    for (const key of Object.keys(errors)) {
-      switch (key) {
-        case 'required':
-          return 'Este campo es requerido'
-
-          case 'minlength':
-            return `El texto debe contener al menos ${ errors['minlength'].requiredLength } caracteres`
-
-          case 'min':
-            return `El valor mínimo debe ser ${ errors['min'].min }`
-      }
-    }
-
-    return null;
-  }
 
   onSubmit() {
     if (this.myForm.invalid) {
