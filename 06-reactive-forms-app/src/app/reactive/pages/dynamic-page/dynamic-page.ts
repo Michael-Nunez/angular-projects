494a5c7e0c36c645ec813627744d71bce6fab069
Michael Nunez <michaeln@labreferencia.com>
2025-10-27 11:18:02 -0400

    Agregando un listado de valores (nombres de juegos) mediante un input

--- 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,9 +1,49 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.html'
 })
-export class DynamicPage { }
+export class DynamicPage {
+  private formBuilder = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.formBuilder.array([
+      //* Aquí van las reglas de los campos
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ],
+    //* Aquí van las reglas del arreglo
+    Validators.minLength(3)),
+  });
+
+  newFavoriteGame = new FormControl('', Validators.required);
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onAddToFavorites() {
+    if (this.newFavoriteGame.invalid) return;
+
+    const newGame = this.newFavoriteGame.value;
+
+    this.favoriteGames.push(this.formBuilder.control(newGame, Validators.required));
+
+    this.newFavoriteGame.reset();
+  }
+
+  onDeleteFavoriteGame(index: number) {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
--- 06-reactive-forms-app/src/app/utils/form-utils.ts
+++ 06-reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,15 +1,11 @@
-import { FormGroup } from "@angular/forms";
+import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
 export class FormUtils {
   static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return !! (form.controls[fieldName].errors && form.controls[fieldName].touched);
   }
 
-  static getFieldErrors(form: FormGroup, fieldName: string): string | null {
-    if (!form.controls[fieldName]) return null;
-
-    const errors = form.controls[fieldName].errors ?? {};
-
+  static getTextErrors(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -24,5 +20,27 @@ export class FormUtils {
     }
 
     return null;
+  }
+
+  static getFieldErrors(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
+
+    const errors = form.controls[fieldName].errors ?? {};
+
+    return FormUtils.getTextErrors(errors);
+  }
+
+  static getFieldErrorsInArray(formArray: FormArray, index: number): string | null {
+    if (formArray.controls.length === 0) return null;
+
+    const errors = formArray.controls[index].errors ?? {};
+
+    return FormUtils.getTextErrors(errors);
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
   }
 }
